3958cacb9b34d3834766ea19213a66fa6d22dbf5
ManikPratapwar <87516218+ManikPratapwar@users.noreply.github.com>
2023-09-15 14:49:06 +0530

    Craft class Created

--- Incubyte.cpp
+++ Incubyte.cpp
@@ -1,5 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-    
-}
+class craft
+{
+private:
+    int x, y, z;    // Current position (x, y, z)
+    char direction; // Current direction (N, S, E, W, U, D)
+    char origin;
+
+public:
+
+    // Declaring Constructor craft with x,y,z co-ordinates and directon.
+    craft(int &initialX, int &initialY, int &initialZ, char &initialDirection, char org)
+    {
+        x = initialX;
+        y = initialY;
+        z = initialZ;
+        direction = initialDirection;
+        origin = org;
+    }
+};
