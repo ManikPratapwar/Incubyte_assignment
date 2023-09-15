c36eb874b9a2015955be80f807c84e61a7d453f9
ManikPratapwar <87516218+ManikPratapwar@users.noreply.github.com>
2023-09-15 14:51:36 +0530

    Left, Right, Up, Down Functions added

--- Incubyte.cpp
+++ Incubyte.cpp
@@ -69,4 +69,169 @@ public:
             break;
         }
     }
+    void left() // l
+    {
+        switch (direction)
+        {
+        case 'N':
+            direction = 'W';
+            origin = 'W';
+            break;
+        case 'S':
+            direction = 'E';
+            origin = 'E';
+            break;
+        case 'E':
+            direction = 'N';
+            origin = 'N';
+            break;
+        case 'W':
+            direction = 'S';
+            origin = 'S';
+            break;
+        case 'D':
+            if (origin == 'N')
+            {
+                direction = 'W';
+                origin = 'W';
+                break;
+            }
+            else if (origin == 'S')
+            {
+                direction = 'E';
+                origin = 'E';
+                break;
+            }
+            else if (origin == 'E')
+            {
+                direction = 'N';
+                origin = 'N';
+                break;
+            }
+            else
+            {
+                direction = 'S';
+                origin = 'S';
+                break;
+            }
+        case 'U':
+            if (origin == 'N')
+            {
+                direction = 'W';
+                origin = 'W';
+                break;
+            }
+            else if (origin == 'S')
+            {
+                direction = 'E';
+                origin = 'E';
+                break;
+            }
+            else if (origin == 'E')
+            {
+                direction = 'N';
+                origin = 'N';
+                break;
+            }
+            else
+            {
+                direction = 'S';
+                origin = 'S';
+                break;
+            }
+        }
+    }
+
+    // Moving craft Right
+    void right() // r
+    {
+
+        switch (direction)
+        {
+        case 'N':
+            direction = 'E';
+            origin = 'E';
+            break;
+        case 'S':
+            direction = 'W';
+            origin = 'W';
+            break;
+        case 'E':
+            direction = 'S';
+            origin = 'S';
+            break;
+        case 'W':
+            direction = 'N';
+            origin = 'N';
+            break;
+        case 'D':
+            if (origin == 'N')
+            {
+                direction = 'E';
+                origin = 'E';
+                break;
+            }
+            else if (origin == 'S')
+            {
+                direction = 'W';
+                origin = 'W';
+                break;
+            }
+            else if (origin == 'E')
+            {
+                direction = 'S';
+                origin = 'S';
+                break;
+            }
+            else
+            {
+                direction = 'N';
+                origin = 'N';
+                break;
+            }
+        case 'U':
+            if (origin == 'N')
+            {
+                direction = 'E';
+                origin = 'E';
+                break;
+            }
+            else if (origin == 'S')
+            {
+                direction = 'W';
+                origin = 'W';
+                break;
+            }
+            else if (origin == 'E')
+            {
+                direction = 'S';
+                origin = 'S';
+                break;
+            }
+            else
+            {
+                direction = 'N';
+                origin = 'N';
+                break;
+            }
+        }
+    }
+
+    // Moving craft Up
+    void up() // u
+    {
+        if (direction != 'U')
+        {
+            direction = 'U';
+        }
+    }
+
+    // Moving craft Down
+    void down() // d
+    {
+        if (direction != 'D')
+        {
+            direction = 'D';
+        }
+    }
 };
